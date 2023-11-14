4006739025e3a0eacb726b4d5063412e8df4b57d
Theresa7777 <138199668+Theresa7777@users.noreply.github.com>
2023-11-14 02:23:02 +0100

    Added the github link to the repository with some css touches.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Weather Search
-        </h1>
+        <h1>Weather Search</h1>
         <Weather />
+        <br />
+        <p>
+          <a
+            href="https://github.com/Theresa7777/weather-react2"
+            className="Gitlink"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Theresa Obuladike for a React Project.
+        </p>
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -53,4 +53,5 @@ export default function Weather() {
   } else {
     return form;
   }
+
 }
